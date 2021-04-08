3ea980c94ccb7be3b16ed26f81107465a1f5cdef
V4lonforth <v4lonforth@gmail.com>
2021-04-08 16:32:20 +0300

    Add pause menu

---
+++ Assets/Project/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts.UI.PauseMenu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseButton;
+        [SerializeField] private GameObject resumeButton;
+
+        private void Awake()
+        {
+            SetPauseState(false);
+        }
+
+        public void Pause()
+        {
+            Time.timeScale = 0f;
+            SetPauseState(true);
+        }
+
+        public void Resume()
+        {
+            Time.timeScale = 1f;
+            SetPauseState(false);
+        }
+
+        private void SetPauseState(bool state)
+        {
+            pauseButton.SetActive(!state);
+            resumeButton.SetActive(state);
+        }
+    }
+}
